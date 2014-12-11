ea7fafe938ab2c7f796f301034dece8ecb0d9fc4
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-10 18:35:54 -0800

    Implement hat.prototype.shake
    Randomly shuffles hat.contents
    Also update to loop logic in hat.prototype.toString

--- dfah.js
+++ dfah.js
@@ -6,13 +6,34 @@ hat.prototype = {
   add: function(item){
     //TODO: Implement
   },
+
   remove: function(index){
     //TODO: Implement
   },
+
+  shake: function(){
+    //randomize order of contents using Fisher-Yates Shuffle
+    var counter = this.contents.length,
+        temp,
+        counter;
+
+    while (counter){
+      index = Math.floor(Math.random() * counter);
+      counter--;
+
+      //Swap the random-index value with counter value
+      temp = this.contents[index];
+      this.contents[counter] = this.contents[index];
+      this.contents[index] = temp;
+    }
+    return this;
+  },
+
   toString: function(){
-    var str = "";
-    var len = this.contents.length;
-    while(len--){
+    var len = this.contents.length,
+    str = "";
+
+    while (len--){
       console.log(len);
       str += this.contents[len] + "\n"; 
     }
