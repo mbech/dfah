56779ada452d9635d5a3e24ac499e3c8addac813
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-12-13 20:34:57 -0800

    Handle empty text form submission

--- domManip.js
+++ domManip.js
@@ -3,7 +3,11 @@ DFAH.domManip = {
     var $inputField = $('#hat-text-input');
     $inputField.parent('form').on("submit", function(e){
       e.preventDefault();
-      $inputField.val("").attr("placeholder", "add more!");
+      //check if input is empty
+      if ($inputField.val().trim()){
+        $inputField.val("").attr("placeholder", "add more!");
+        //TODO: add input value to hat contents object
+      }
       $inputField.focus();
     });
   },
