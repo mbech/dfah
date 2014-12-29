5e221fa6a4e26b1e75304631469ebd0cc07135c2
mbech <matt.b.bechtel@gmail.com>
2014-12-28 19:32:48 -0800

    Add test, link scripts in test.html

--- test/tests.js
+++ test/tests.js
@@ -2,3 +2,9 @@
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
+
+//hat.js tests
+QUnit.test("hat.js methods", function(assert) {
+  assert.ok(DFAH.hat, "hat is defined");
+});
+
